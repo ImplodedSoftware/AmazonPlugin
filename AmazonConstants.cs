c0e509c8fadf524a333040006030821f05490734
Mikael Stalvik <mikael@helium.fm>
2016-07-03 13:52:16 +0200

    Indention fixes

--- AmazonConstants.cs
+++ AmazonConstants.cs
@@ -2,7 +2,7 @@ namespace AmazonPlugin
 {
     public class AmazonConstants
     {
-public const string AWS_ACCESS_KEY_ID = "<insert_key>";
-public const string AWS_SECRET_KEY = "<insert_secret>";
+        public const string AWS_ACCESS_KEY_ID = "<insert_key>";
+        public const string AWS_SECRET_KEY = "<insert_secret>";
     }
 }
